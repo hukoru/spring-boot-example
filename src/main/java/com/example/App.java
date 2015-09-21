4ad3c89c0dd61f70573e5ef132047e3210eb006d
hukoru <hukoru@martmoa.com>
2015-09-21 12:10:23 +0900

    framework 설계

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,31 +1,34 @@
 package com.example;
 
-import com.example.app.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
 
-import java.util.Scanner;
+
+import com.example.domain.Customer;
+import com.example.service.CustomerService;
 
 @EnableAutoConfiguration
-@Import(Appconfig.class)
-public class App {
+@ComponentScan
+public class App implements CommandLineRunner {
+    @Autowired
+    CustomerService customerService;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        // 데이터 추가
+        customerService.save(new Customer(1, "Nobita", "Nobi"));
+        customerService.save(new Customer(2, "Takeshi", "Goda"));
+        customerService.save(new Customer(3, "Suneo", "Honekawa"));
+
+        // 데이터 추가
+        customerService.findAll()
+                .forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
-        try(ConfigurableApplicationContext context =
-                SpringApplication.run(App.class, args)){
-
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter 2 number like 'a b' : ");
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-
-            Calculator calculator = context.getBean(Calculator.class);
-            int result = calculator.calc(a, b);
-
-            System.out.println("result = " + result);
-            scanner.close();
-
-        }
+        SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/com/example/app/AugumentResolver.java
+++
@@ -1,7 +0,0 @@
-package com.example.app;
-
-import java.io.InputStream;
-
-public interface AugumentResolver {
-    Augument resolve(InputStream stream);
-}
